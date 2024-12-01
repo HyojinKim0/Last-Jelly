3aeb2cc990749daa41d174ccb682e40332313d13
nahyeonsu <152411843+nahyeonsu@users.noreply.github.com>
2024-12-01 23:20:22 +0900

    1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,4 +5,11 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public Vector3[] pointlist;
+
+    public RuntimeAnimatorController[] LevelAc;
+
+    public void ChangeA(Animator anim, int level)
+    {
+        anim.RuntimeAnimatorController = LevelAc[level - 1];
+    }
 }
