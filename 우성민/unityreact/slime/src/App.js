ac8db03aced9c330a3e4e2bd93b677aee48ab190
seongminWoo2101 <bkshark7@naver.com>
2024-12-04 14:11:08 +0900

    유니티 웹 붙이기
    완성본입니다

--- 우성민/unityreact/slime/src/App.js
+++ 우성민/unityreact/slime/src/App.js
@@ -3,18 +3,17 @@ import './App.css';
 import * as React from 'react';
 import { Unity, useUnityContext } from "react-unity-webgl";
 
-
-const { unityProvider } = useUnityContext({
-  loaderUrl: "Build/build.loader.js",
-  dataUrl: "Build/build.data",
-  frameworkUrl: "Build/build.framework.js",
-  codeUrl: "Build/build.wasm",
-});
-
 function App() {
+
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "Build/build.loader.js",
+    dataUrl: "Build/build.data",
+    frameworkUrl: "Build/build.framework.js",
+    codeUrl: "Build/build.wasm",
+  });
+
   return (
     <div className="App"> 
-        <img src={logo} className="App-logo" alt="logo" />
         <Unity unityProvider={unityProvider}
                 style={{width: '1500px', height: '800px'}}
         />
